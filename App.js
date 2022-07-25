aa8c17eaa88ea60631cc327f9d8b2b3c11fd8355
robert-virtual <robertocastill63@gmail.com>
2022-07-25 16:31:52 -0600

    done

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { API_URL } from "./constantes"
 export default function App() {
+  const [personajes, setPersonajes] = useState([])
+  useEffect(() => {
+    solicitarDatos()
+  }, [])
+
+  async function solicitarDatos() {
+    const res = await fetch(API_URL + "/character")
+    const datos = await res.json()
+    console.log("datos:", datos)
+    setPersonajes(datos.results)
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={personajes}
+        renderItem={({ item }) => (
+          <View style={{ padding: 10,margin:10,backgroundColor:"#c3c3c3" }}>
+            <Text>{item.name}</Text>
+            <Text>{item.status}</Text>
+
+          </View>
+        )}
+      />
     </View>
   );
 }
---
+++ constantes.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://rickandmortyapi.com/api"
+// const API_URL2 = "http://localhost:3000"
